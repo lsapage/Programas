348d4a9c9fe1641f81bf7c9c8783fa0855619860
lsapage <73650081+lsapage@users.noreply.github.com>
2021-02-06 18:50:13 -0300

    Update prog_teste.py

--- prog_teste.py
+++ prog_teste.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import streamlit as st
 
-#lista1 = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
-#lista2 = [11, 22, 33, 44, 55, 66, 77]
+lista1 = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks'] 
+lista2 = [11, 22, 33, 44, 55, 66, 77]
 
-#base = pd.DataFrame(list(zip(lista1, lista2)), 
-#               columns =['Name', 'val'])
+base = pd.DataFrame(list(zip(lista1, lista2)), 
+               columns =['Name', 'val'])
 
 
 st.write("""
@@ -14,4 +14,4 @@ st.write("""
 
 """)
 
-#st.dataframe(base)
+st.dataframe(base)
